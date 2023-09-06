a511eda6ab64e62ea92df89862ea0ced47ebd8eb
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-09-06 15:43:33 -0300

    feat: create a request function

--- src/services/request.py
+++ src/services/request.py
@@ -1,5 +1,13 @@
 import requests
 
 def geoRequest(token, ip):
-    body = requests.get(f'https://geo.ipify.org/api/v2/country,city?apiKey={token}&ipAddress={ip}')
-    return body.content
+    """
+        Função responsável pela comunicação com o endpoit de IP;
+    """
+    try:
+        body = requests.get(f'https://geo.ipify.org/api/v2/country,city?apiKey={token}&ipAddress={ip}')
+        return body.content
+    except:
+        erro = 'Token Inválido ou expirado!'
+        print(erro)
+        return erro
